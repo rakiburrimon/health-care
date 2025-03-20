20727a13db80df02d000d851dc687c44cb971ad8
Md. Rakibur Rahman <rakiburrimon@gmail.com>
2025-03-20 10:13:12 +0600

    feat: Prepare auth with passport and migrations

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // Here is where you define the route middleware
+        $middleware->alias([
+            'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
+            'auth:api',
+            \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
